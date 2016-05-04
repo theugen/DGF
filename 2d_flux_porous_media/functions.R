862d19a9d60008f7f257c897e2f167086f55f487
Eugenio Thieme <eugen@thieme.it>
2016-05-04 08:20:29 +0200

    Ancora sul numero di iterazioni
    Il problema è ancora sul numero di iterazioni, esce dopo averne fatta solo una...

--- 2d_flux_porous_media/functions.R
+++ 2d_flux_porous_media/functions.R
@@ -16,9 +16,10 @@ set_dirichlet <- function(x, a, b){
 get_h <- function(x, threshold = 1e-6){
   #diff <- 500
   #diff <- x[1,1]
-  #differ <- 100
-  counter <- 0
-  while(100 > 1){
+  previous <- x[floor(length(x[1,])/2), floor(length(x[,1])/2)]
+  differ <- x[floor(length(x[1,])/2), floor(length(x[,1])/2)]
+  #counter <- 0
+  if(differ ==0){
     for(i in 1: length(x[,1])){  # Cycling over rows
       for(j in 2:(length(x[1,])-1)){ # Cycling over columns
         if(i != 1 && i!=length(x[,1])){
@@ -32,12 +33,32 @@ get_h <- function(x, threshold = 1e-6){
         }
       }
     }
-    if(counter == 0) differ <- x[floor(length(x[1,])/2), floor(length(x[,1])/2)]
-    if((abs(differ - x[floor(length(x[1,])/2), floor(length(x[,1])/2)])) < threshold) break
-    #differ <- differ - x[floor(length(x[1,])/2), floor(length(x[,1])/2)]
-    #cat(paste(abs(differ), '\n'))
-    counter <- counter + 1
+  } 
+while(differ > threshold){
+  for(i in 1: length(x[,1])){  # Cycling over rows
+    for(j in 2:(length(x[1,])-1)){ # Cycling over columns
+      if(i != 1 && i!=length(x[,1])){
+        x[i,j] <- (x[i+1,j] + x[i-1,j] + x[i, j+1] + x[i, j-1])/4
+      }
+      else if(i==1){
+        x[i,j] <- (x[i+1,j] + x[i, j+1] + x[i, j-1])/3
+      }
+      else if(i==length(x[,1])){
+        x[i,j] <- (x[i-1,j] + x[i, j+1] + x[i, j-1])/3
+      }
+    }
   }
+    now <- x[floor(length(x[1,])/2), floor(length(x[,1])/2)]
+    #temp <- abs(differ - x[floor(length(x[1,])/2), floor(length(x[,1])/2)])
+    differ <- abs(previous - now)
+    #if(counter == 0) differ <- x[floor(length(x[1,])/2), floor(length(x[,1])/2)]
+    #if((abs(differ - x[floor(length(x[1,])/2), floor(length(x[,1])/2)])) < threshold) break
+    #differ <- differ - x[floor(length(x[1,])/2), floor(length(x[,1])/2)]
+    previous <- now
+    cat(paste(abs(differ), '\n'))
+    #counter <- counter + 1
+  }
+  
   #rm(diff)
 #   cat(paste(abs(differ), '\n'))
   x
